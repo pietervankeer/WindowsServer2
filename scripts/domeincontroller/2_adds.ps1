7586774317abc1fb16565d88d2c389eb7781f827
Pieter Van Keer <pieter.vankeer@student.hogent.be>
2021-10-07 16:29:27 +0200

    add script for installing and config ADDS

---
+++ scripts/domeincontroller/2_adds.ps1
@@ -0,0 +1,56 @@
+#------------------------------------------------------------------------------
+# Domaincontroller
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+# Variables
+#------------------------------------------------------------------------------
+
+$generalSettings = Get-Content -Path "../settings.json" | ConvertFrom-Json
+$settings = Get-Content -Path settings.json | ConvertFrom-Json
+
+$domainName = $generalSettings.ADDS.domainName
+$netBiosName = $generalSettings.ADDS.netBiosName
+$password = $generalSettings.ADDS.password
+
+
+#------------------------------------------------------------------------------
+# Installation ADDS
+#------------------------------------------------------------------------------
+
+Try {
+
+    Install-WindowsFeature AD-Domain-Services -IncludeManagementTools -Restart -ErrorAction Stop
+    Write-Host "Active Directory Domain Services installed successfully" -ForegroundColor Green
+
+} Catch {
+
+    Write-Warning -Message $("Failed to install Active Directory Domain Services. Error: "+ $_.Exception.Message)
+}
+
+#------------------------------------------------------------------------------
+# Promotion to domain controller
+#------------------------------------------------------------------------------
+
+try {
+    Import-Module ADDSDeployment
+    Install-ADDSForest `
+    -CreateDnsDelegation:$false `
+    -DatabasePath "C:\Windows\NTDS" `
+    -DomainMode "WinThreshold" `
+    -DomainName $domainName `
+    -DomainNetbiosName $netBiosName `
+    -ForestMode "WinThreshold" `
+    -InstallDns:$true `
+    -LogPath "C:\Windows\NTDS" `
+    -NoRebootOnCompletion:$false `
+    -SysvolPath "C:\Windows\SYSVOL" `
+    -SafeModeAdministratorPassword (ConvertTo-SecureString -String $password -AsPlainText -Force) `
+    -Force:$true
+    Write-Host "Active Directory Domain Services have been configured successfully" -ForegroundColor Green
+}
+catch {
+    Write-Warning -Message $("Failed to configure Active Directory Domain Services. Error: "+ $_.Exception.Message)
+}
+
+Pause
