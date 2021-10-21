bb27a624b56d6d97b1ab2abb383e6109ddb60f03
Pieter Van Keer <pieter.vankeer@student.hogent.be>
2021-10-21 12:52:24 +0200

    restart computer after domain join

--- scripts/algemeen/join_domain.ps1
+++ scripts/algemeen/join_domain.ps1
@@ -1,13 +1,24 @@
 
 $domaincontrollersettings = Get-Content -Path ../domeincontroller/settings.json | ConvertFrom-Json
+$algemeneSettings = Get-Content -Path ../settings.json | ConvertFrom-Json
 
 # default gateway
 $defaultGateway = $domaincontrollersettings.Network[1].IPAdress
 
-# domain join
-$password = ConvertTo-SecureString $generalsettings.ADDS.password -AsPlainText -Force
+# prepare domain join
+$password = ConvertTo-SecureString $algemeneSettings.ADDS.password -AsPlainText -Force
 $Cred = New-Object System.Management.Automation.PSCredential ("Administrator", $password)
+$domainName = $algemeneSettings.ADDS.DomainName
 
 
 # Domein joinen
-Add-Computer -DomainName EP1-PIETER.hogent -OUPath "OU=MemberServers,DC=EP1-PIETER,DC=hogent" -Credential $Cred
+try {
+    Add-Computer -DomainName $domainName -OUPath "OU=MemberServers,DC=EP1-PIETER,DC=hogent" -Credential $Cred
+    Write-Host "Joined Domein $domainName" -ForegroundColor Green
+}
+catch {
+    Write-Warning -Message $("Failed to join domain $domainName. Error: "+ $_.Exception.Message)
+}
+
+Pause
+Restart-Computer
