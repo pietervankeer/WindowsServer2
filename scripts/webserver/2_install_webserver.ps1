f45fece1b16bc4029487e0495511b430fe6b76df
Pieter Van Keer <pieter.vankeer@student.hogent.be>
2021-10-28 16:04:31 +0200

    replace html file for webserver

--- scripts/webserver/2_install_webserver.ps1
+++ scripts/webserver/2_install_webserver.ps1
@@ -9,6 +9,26 @@
 $generalSettings = Get-Content -Path "../settings.json" | ConvertFrom-Json
 $settings = Get-Content -Path settings.json | ConvertFrom-Json
 
+$webserverPath = "C:\inetpub\wwwroot"
+
+$html = '
+<!DOCTYPE html>
+<html>
+<head>
+<title>IIS Windows Server</title>
+<style type="text/css">
+body {
+	color:#000000;
+	background-color:#0072C6;
+	margin:0;
+}
+</style>
+</head>
+<body>
+	<h1>Hallo, Welkom op de webserver in het domein EP1-PIETER.HoGent</h1>
+</body>
+</html>
+'
 
 #------------------------------------------------------------------------------
 # Installation Webserver role
@@ -16,21 +36,35 @@ $settings = Get-Content -Path settings.json | ConvertFrom-Json
 
 # Install roles
 try {
-    $job = @()
+    $jobs = @()
     $jobs += start-Job -Command {
-        Install-WindowsFeature –ConfigurationFilePath F:\configfiles\install_webserver.xml
+        Install-WindowsFeature -configurationFilePath F:\configfiles\install_webserver.xml
     }
     Receive-Job -Job $jobs -Wait | select-Object Success, RestartNeeded, exitCode, FeatureResult
-    Write-Host "webserver successfully installed" -ForegroundColor Green
+    Write-Host "Webserver succesfully installed" -ForegroundColor Green
 }
 catch {
-    Write-Warning -Message $("Failed to install webserver. Error: "+ $_.Exception.Message)
+    Write-Warning -Message $("Failed to install Webserver. Error: " + $_.Exception.Message)
 }
 
 #------------------------------------------------------------------------------
 # Configuration Webserver
 #------------------------------------------------------------------------------
 
-# TODO
+# replace existing html file.
+try {
+    # remove html file
+    rm $webserverPath\iisstart.*
+
+    # make new html file
+    New-Item $webserverPath\index.html -ItemType File
+    # add html to file
+    Set-Content $webserverPath\index.html $html
+
+    Write-Host "html file succesfully removed." -ForegroundColor Green
+}
+catch {
+    Write-Warning -Message $("Failed to configure Webserver. Error: " + $_.Exception.Message)
+}
 
 Pause
