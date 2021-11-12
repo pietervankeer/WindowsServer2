4bf8502f37e31dfdcf65f1d8b70c737c4e710d7e
Pieter Van Keer <pieter.vankeer@student.hogent.be>
2021-11-12 11:55:11 +0100

    add mdt install script

--- scripts/deploymentserver/2_install_mdt.ps1
+++ scripts/deploymentserver/2_install_mdt.ps1
@@ -11,17 +11,11 @@ $file="F:\configfiles\install_mdt.msi"
 
 # install mdt.msi
 try {
-    $DataStamp = get-date -Format yyyyMMddTHHmmss
-    $logFile = '{0}-{1}.log' -f $file.fullname,$DataStamp
     $MSIArguments = @(
         "/i"
-        ('"{0}"' -f $file.fullname)
-        "/qn"
-        "/norestart"
-        "/L*v"
-        $logFile
+        ('"{0}"' -f $file)
     )
-    Start-Process "msiexec.exe" -ArgumentList $MSIArguments -Wait -NoNewWindow 
+    Start-Process "msiexec.exe" -ArgumentList $MSIArguments -Wait 
     Write-Host 'Microsoft Deployment Toolkit successfully installed' -ForegroundColor Green
 } catch {
     Write-Warning -Message $("Failed to install Microsoft Deployment Toolkit. Error: " + $_.Exception.Message)
