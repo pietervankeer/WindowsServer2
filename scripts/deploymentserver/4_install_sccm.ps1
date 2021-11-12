421d4bd8030918bb050a8f7b7aeda24c8163f90a
Pieter Van Keer <pieter.vankeer@student.hogent.be>
2021-11-12 11:56:50 +0100

    Fix typo

--- scripts/deploymentserver/4_install_sccm.ps1
+++ scripts/deploymentserver/4_install_sccm.ps1
@@ -6,18 +6,18 @@
 # Variables
 #------------------------------------------------------------------------------
 
-# Pad naar de installer voor adk
-$file="F:\configfiles\install_adk.exe"
+# Pad naar de installer voor sccm
+$file="F:\configfiles\install_sccm.exe"
 
-# install adk.exe
+# install sccm.exe
 try {
     $EXEArguments = @(
         ('"{0}"' -f $file)
     )
     Start-Process "$file" -Wait 
-    Write-Host 'Microsoft ADK successfully installed' -ForegroundColor Green
+    Write-Host 'Microsoft SCCM successfully installed' -ForegroundColor Green
 } catch {
-    Write-Warning -Message $("Failed to install Microsoft ADK. Error: " + $_.Exception.Message)
+    Write-Warning -Message $("Failed to install Microsoft SCCM. Error: " + $_.Exception.Message)
 }
 
 Pause
